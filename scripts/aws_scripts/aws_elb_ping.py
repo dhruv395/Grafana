663d1ef473e937da8ecdd342d587c530229a4d28
Dhruba Jyoti Ray <36419707+dhruv395@users.noreply.github.com>
2019-09-20 23:20:06 +0530

    Create aws_elb_ping.py

---
+++ scripts/aws_scripts/aws_elb_ping.py
@@ -0,0 +1,34 @@
+import requests
+import socket
+from influxdb import InfluxDBClient
+from datetime import datetime
+
+url = "http://prod-2053689399.us-west-2.elb.amazonaws.com/cgi-bin/ciwweb.pl"
+
+client = InfluxDBClient('influxdb-1160877343.us-west-2.elb.amazonaws.com', 8086, 'username', 'password', 'telegraf_metrics')
+json_body = [
+			    {
+			        "measurement": "ping",
+			        "tags": {
+			            "host": socket.gethostname(),
+			            "url": url
+			        },
+			        "time": datetime.now().isoformat(),
+			        "fields": {
+			            "result_code": -1,
+			            "url": url
+			        }
+			    }
+			]
+
+try:
+	if requests.get(url).status_code == 200:
+		json_body[0]['fields']['result_code'] = 0
+	else:
+		json_body[0]['fields']['result_code'] = 1
+except requests.exceptions.RequestException:
+	json_body[0]['fields']['result_code'] = 1
+
+
+# writing to influxdb
+client.write_points(json_body)
